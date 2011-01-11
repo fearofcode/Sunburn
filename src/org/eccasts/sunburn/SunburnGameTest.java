6ce88227444bc05e6ab7894f1c49a0c8f2b2ecad
Warren Henning <warren.henning@gmail.com>
2011-01-11 04:15:06 -0800

    Basic combat works

---
+++ src/org/eccasts/sunburn/SunburnGameTest.java
@@ -0,0 +1,251 @@
+package org.eccasts.sunburn;
+
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class SunburnGameTest {
+    private SunburnShip ship;
+
+    @Before public void init() {
+        ship = new SunburnShip(new Random());
+        ship.setGenome("GLMDSSMGLSDSMLGMLSDS");
+    }
+    
+    @Test public void GenerateRandomShip() {
+       int dCount = 0;
+       
+       for(int i = 1; i <= 50000; i++) {
+           String genome = ship.newRandomGenome();
+           
+           assertEquals(20, genome.length());
+           for(int j = 0; j < 20; j++)
+               if(genome.charAt(j) == 'D')
+                   dCount++;
+           
+       }
+
+       assertTrue(dCount >= 195000 && dCount <= 205000);
+    }
+    
+    @Test public void PreferredRange() {
+       assertEquals(20, ship.getPreferredRange());
+       
+       boolean threwException = false;
+       
+       try {
+           ship.setPreferredRange(31);
+       } catch(IllegalArgumentException e) {
+           threwException = true;
+       }
+       
+       assertTrue(threwException);
+       
+       threwException = false;
+       
+       try {
+           ship.setPreferredRange(0);
+       } catch(IllegalArgumentException e) {
+           threwException = true;
+       }
+       
+       assertTrue(threwException);
+    }
+    
+    @Test public void DevelopShipGenome() {
+        assertEquals("SSSSSSSSSSSSSSSSSSGLMDMGLDMLGMLD", ship.developGenome());
+    }
+    
+    @Test public void CountSystems() {
+        assertEquals(3, ship.gunCount());
+        assertEquals(4, ship.laserCount());
+        assertEquals(4, ship.missileCount());
+        assertEquals(18, ship.shieldCount());
+        assertEquals(3, ship.driveCount());
+    }
+    
+    @Test public void weaponProbabilities() {
+        assertEquals(0.6, ship.hitProbabilityGun(1), 0.01);
+        assertEquals(-0.4, ship.hitProbabilityGun(39), 0.01);
+        
+        for(int i = 1; i <= 30; i++)
+            assertEquals(0.35, ship.hitProbabilityLaser(i), 0.01);
+        
+        assertEquals(0.1, ship.hitProbabilityMissile(1), 0.01);
+        assertEquals(-0.9, ship.hitProbabilityMissile(-37), 0.01);
+    }
+    
+    @Test public void FireWeapons() {
+        ship.setRng(new FixedRandom(0.7));
+        assertEquals(0, ship.fireGuns(1));
+        
+        ship.setRng(new FixedRandom(0.5));
+        
+        assertEquals(3, ship.fireGuns(1));
+        
+        ship.setRng(new Random());
+        
+        int hitCount = 0;
+        
+        for(int i = 1; i <= 10000; i++) {
+            hitCount += ship.fireGuns(1);
+        }
+        
+        assertTrue(hitCount >= 17000 && hitCount <= 19000);
+        
+        hitCount = 0;
+        
+        for(int i = 1; i <= 10000; i++) {
+            hitCount += ship.fireMissiles(1);
+        }
+        
+        assertTrue(hitCount >= 3000 && hitCount <= 5000);
+        
+        hitCount = 0;
+        
+        for(int i = 1; i <= 10000; i++) {
+            hitCount += ship.fireLasers(1);
+        }
+        
+        assertTrue(hitCount >= 13000 && hitCount <= 15000);
+        
+        hitCount = 0;
+        
+        for(int i = 1; i <= 10000; i++) {
+            hitCount += ship.fireWeapons(1);
+        }
+        
+        assertTrue(hitCount >= 33000 && hitCount <= 39000);
+    }
+    
+    @Test public void TakeHits() {
+        assertEquals("SSSSSSSSSSSSSGLMDMGLDMLGMLD", ship.takeHits(5));
+        assertEquals("SSSSSSSSSSSSGLMDMGLDMLGMLD", ship.takeHits(1));
+        
+        assertEquals("", ship.takeHits("SSSSSSSSSSSSGLMDMGLDMLGMLD".length()+1));
+    }
+    
+    @Test public void AnyDrivesLeft() {
+        assertTrue(ship.drivesLeft());
+        
+        ship.setGenome("GLMDSSMGLSMSMLGMLSMS");
+        ship.takeHits(6*3+4);
+        
+        assertFalse(ship.drivesLeft());
+    }
+    
+    @Test public void FiringRemovesDrives() {
+        // SSSSSSSSSSSSSSSSSSGLMDMGLDMLGMLD
+        ship.takeHits("SSSSSSSSSSSSSSSSSS".length());
+        assertEquals(3, ship.gunCount());
+        ship.takeHits(1);
+        assertEquals(2, ship.gunCount());
+    }
+    
+    @Test public void MoveShip() {
+        ship.setPreferredRange(25);
+        assertEquals(20, ship.getRange());
+        
+        ship.move();
+        
+        assertEquals(21, ship.getRange());
+        
+        ship.move();
+        
+        assertEquals(22, ship.getRange());
+        
+        ship.setPreferredRange(22);
+        
+        ship.move();
+        
+        assertEquals(22, ship.getRange());
+        
+        ship.setPreferredRange(21);
+        
+        ship.move();
+        
+        assertEquals(21, ship.getRange());
+    }
+    
+    @Test public void UseDrivesToMove() {
+        // SSSSSSSSSSSSSSSSSSGLMDMGLDMLGMLD
+        ship.setPreferredRange(24);
+        ship.moveWithDrives();
+        assertEquals(23, ship.getRange());
+        
+        ship.moveWithDrives();
+        assertEquals(24, ship.getRange());
+        
+    }
+    
+    @Test public void IsDestroyed() {
+        assertEquals(false, ship.isDestroyed());
+        ship.takeHits("SSSSSSSSSSSSSSSSSSGLMDMGLDMLGMLD".length());
+        
+        assertEquals(true, ship.isDestroyed());
+        
+        ship.takeHits(1);
+        
+        assertEquals(true, ship.isDestroyed());
+    }
+    
+    @Test public void CanWin() {
+        // SSSSSSSSSSSSSSSSSSGLMDMGLDMLGMLD
+        
+        ship.setGenome("GLMDSSMGLSMSMLGMLSMS");
+        
+        SunburnShip enemy = new SunburnShip(new Random());
+        enemy.setGenome("GLMDSSMGLSDSMLGMLSDS");
+        enemy.takeHits("SSSSSSSSSSSSSSSSSS".length());
+        assertEquals(false, ship.wonAgainst(enemy));
+        enemy.takeHits("GLMDMGLDMLGMLD".length());
+        assertEquals(true, ship.wonAgainst(enemy));
+        
+        ship.takeHits(6*3+4);
+        
+        assertEquals(false, ship.wonAgainst(enemy));
+    }
+    
+    @Test public void CanLose() {
+        SunburnShip enemy = new SunburnShip(new Random());
+        enemy.setGenome("GLMDSSMGLSDSMLGMLSDS");
+        ship.takeHits("SSSSSSSSSSSSSSSSSSGLMDMGLDMLGMLD".length());
+        
+        assertEquals(true, ship.lostAgainst(enemy));
+    }
+    
+    @Test public void CanDraw() {
+        SunburnShip enemy = new SunburnShip(new Random());
+        enemy.setGenome("GLMDSSMGLSDSMLGMLSDS");
+        ship.takeHits("SSSSSSSSSSSSSSSSSSGLMDMGLDMLGMLD".length());
+        enemy.takeHits("SSSSSSSSSSSSSSSSSSGLMDMGLDMLGMLD".length());
+        
+        assertEquals(true, ship.drewAgainst(enemy));
+    }
+    
+    @Test public void RunCombat() {
+        SunburnShip enemy = new SunburnShip(new Random());
+        enemy.setGenome("SSSSSSSSSSSSSSSSSSSGD");
+        
+        assertEquals(1, ship.fightAgainst(enemy));
+        
+        // against two all-shield setups, draw
+        
+        enemy.setGenome("SSSSSSSSSSSSSSSSSSSS");
+        ship.setGenome("SSSSSSSSSSSSSSSSSSSS");
+        
+        assertEquals(0, ship.fightAgainst(enemy));
+    }
+    
+    class FixedRandom extends Random
+    {
+        private static final long serialVersionUID = 1L;
+        private double value;
+        public FixedRandom(double value) { this.value = value; }
+        public double nextDouble() { return value; }
+    }
+}
---
+++ src/org/eccasts/sunburn/SunburnShip.java
@@ -0,0 +1,275 @@
+package org.eccasts.sunburn;
+
+import java.util.Random;
+import java.util.logging.Logger;
+
+public class SunburnShip {
+    private static final int SHIELD_MULTIPLICATION_FACTOR = 3;
+    public static final int MAX_PREFERRED_RANGE = 30;
+    public static final int MIN_PREFERRED_RANGE = 1;
+    public static final int INITIAL_PREFERRED_RANGE = 20;
+    private Random rng;
+    private int preferredRange;
+    private String genome;
+    private static final char[] ALPHABET = new char[] { 'D', 'S', 'G', 'L', 'M' };
+    private static final int GENOME_LENGTH = 20;
+    private String currentState;
+    private int totalHits;
+    private int range;
+    
+    public SunburnShip(Random rng) {
+        this.rng = rng;
+        this.preferredRange = INITIAL_PREFERRED_RANGE;
+        this.range = INITIAL_PREFERRED_RANGE;
+    }
+
+    public String newRandomGenome() {
+        StringBuffer sb = new StringBuffer(GENOME_LENGTH);
+        
+        for(int i = 1; i <= 20; i++)
+            sb.append(ALPHABET[rng.nextInt(ALPHABET.length)]);
+        
+        return sb.toString();
+    }
+
+    public int getPreferredRange() {
+        return preferredRange;
+    }
+
+    public void setPreferredRange(int preferredRange) {
+        if(preferredRange >= MIN_PREFERRED_RANGE && preferredRange <= MAX_PREFERRED_RANGE)
+            this.preferredRange = preferredRange;
+        else
+            throw new IllegalArgumentException();
+    }
+
+    public void setGenome(String genome) {
+        this.genome = genome;
+        
+        this.currentState = developGenome();
+    }
+
+    public String developGenome() {
+        StringBuffer developed = new StringBuffer();
+        
+        int shieldCount = 0;
+        
+        for(int i = 0; i < genome.length(); i++) {
+            if(genome.charAt(i) == 'S')
+                shieldCount++;
+        }
+        
+        for(int i = 1; i <= shieldCount*SHIELD_MULTIPLICATION_FACTOR; i++)
+            developed.append('S');
+        
+        for(int i = 0; i < genome.length(); i++) {
+            char ch = genome.charAt(i);
+            
+            if(ch != 'S')
+                developed.append(ch);
+        }
+        
+        return developed.toString();
+    }
+
+    private int systemCount(char system) {
+        int characterCount = 0;
+        
+        for(int i = 0; i < currentState.length(); i++) {
+            char ch = currentState.charAt(i);
+            
+            if(ch == system)
+                characterCount++;
+        }
+        return characterCount;
+    }
+
+    public int driveCount() {
+        return systemCount('D');
+    }
+
+    public int gunCount() {
+        return systemCount('G');
+    }
+
+    public int laserCount() {
+        return systemCount('L');
+    }
+
+    public int missileCount() {
+        return systemCount('M');
+    }
+
+    public int shieldCount() {
+        return systemCount('S');
+    }
+
+    public double hitProbabilityGun(int range) {
+        return 0.6 - (range-1.0)/38.0;
+    }
+
+    public double hitProbabilityLaser(int range) {
+        return 0.35;
+    }
+
+    public double hitProbabilityMissile(int range) {
+        return 0.1 + (range - 1.0)/38.0;
+    }
+
+    public void setRng(Random rng) {
+        this.rng = rng;
+    }
+
+    public int fireGuns(int range) {
+        int hitCount = 0;
+        
+        for(int i = 1; i <= gunCount(); i++) {
+            double probability = hitProbabilityGun(range);
+            
+            if(rng.nextDouble() < probability) {
+                hitCount++;
+            }
+        }
+        
+        return hitCount;
+    }
+
+    public int fireMissiles(int range) {
+        int hitCount = 0;
+        
+        for(int i = 1; i <= missileCount(); i++) {
+            double probability = hitProbabilityMissile(range);
+            
+            if(rng.nextDouble() < probability) {
+                hitCount++;
+            }
+        }
+        
+        return hitCount;
+    }
+
+    public int fireLasers(int range) {
+        int hitCount = 0;
+        
+        for(int i = 1; i <= laserCount(); i++) {
+            double probability = hitProbabilityLaser(range);
+            
+            if(rng.nextDouble() < probability) {
+                hitCount++;
+            }
+        }
+        
+        return hitCount;
+    }
+
+    public int fireWeapons(int range) {
+        return fireGuns(range) + fireMissiles(range) + fireLasers(range);
+    }
+
+    public String takeHits(int count) {
+        totalHits += count;
+        
+        String developed = developGenome();
+        
+        totalHits = Math.min(developed.length(), totalHits);
+        
+        currentState = developed.substring(totalHits);
+        
+        return currentState;
+    }
+
+    public String getCurrentState() {
+        return currentState;
+    }
+    
+    public boolean drivesLeft() {
+        return currentState.indexOf('D') != -1;
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public void move() {
+        if(range < preferredRange) {
+            range++;
+        } else if(range > preferredRange) {
+            range--;
+        }
+    }
+
+    public void moveWithDrives() {
+        int movesToMake = Math.min(driveCount(), Math.abs(getRange()-getPreferredRange()));
+        
+        for(int i = 1; i <= movesToMake; i++)
+            move();
+    }
+
+    public boolean isDestroyed() {
+        return !(gunCount() > 0 || missileCount() > 0 || laserCount() > 0);
+    }
+
+    public boolean wonAgainst(SunburnShip enemy) {
+        return !isDestroyed() && drivesLeft() && enemy.isDestroyed();
+    }
+
+    public boolean lostAgainst(SunburnShip enemy) {
+        return !enemy.isDestroyed() && enemy.drivesLeft() && isDestroyed();
+    }
+
+    public boolean drewAgainst(SunburnShip enemy) {
+        return !(drivesLeft() || enemy.drivesLeft()) || 
+                (isDestroyed() && enemy.isDestroyed());
+    }
+
+    public synchronized int fightAgainst(SunburnShip enemy) {
+        int roundCount = 1;
+        
+        Logger logger = Logger.getLogger(SunburnShip.class.getName());
+        
+        while(true) {
+            if(roundCount > 100) {
+                logger.info("100 rounds played, calling it a draw");
+                return 0;
+            }
+             
+            logger.info("Round #" + roundCount);
+            
+            int ownHits = fireWeapons(range);
+            int enemyHits = enemy.fireWeapons(enemy.getRange());
+            
+            logger.info("I got " + ownHits + " hits against his " + enemyHits);
+            
+            takeHits(enemyHits);
+            enemy.takeHits(ownHits);
+            
+            logger.info("Me: " + getCurrentState());
+            logger.info("Him: " + enemy.getCurrentState());
+            
+            if(wonAgainst(enemy)) {
+                logger.info("I win!");
+                logger.info("My final state: " + getCurrentState());
+                logger.info("His final state: " + enemy.getCurrentState());
+                return 1;
+            } else if(lostAgainst(enemy)) {
+                logger.info("I lose!");
+                logger.info("My final state: " + getCurrentState());
+                logger.info("His final state: " + enemy.getCurrentState());
+                return -1;
+            } else if(drewAgainst(enemy)) {
+                logger.info("Stalemate.");
+                logger.info("My final state: " + getCurrentState());
+                logger.info("His final state: " + enemy.getCurrentState());
+                return 0;
+            }
+            
+            moveWithDrives();
+            enemy.moveWithDrives();
+            
+            logger.info("My current range: " + getRange());
+            logger.info("His current range: " + enemy.getRange());
+            
+            roundCount++;
+        }
+    }
+}
